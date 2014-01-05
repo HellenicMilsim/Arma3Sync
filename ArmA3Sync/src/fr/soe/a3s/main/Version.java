bf9c12f81b9897e7086ca5d728d3f7a979d6a94e
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2014-01-05 11:15:15 +0000

    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/releases@762 73b07b11-db6f-9345-a1c1-cd074a5028ff

---
+++ ArmA3Sync/src/fr/soe/a3s/main/Version.java
@@ -0,0 +1,30 @@
+package fr.soe.a3s.main;
+
+public class Version {
+
+    private static final String NAME = "1.1 Update 6";
+
+    private static final int MAJOR = 1;
+
+    private static final int MINOR = 1;
+
+    private static final int BUILD = 30;
+
+    private static final String YEAR = "2013";
+
+    public static String getVersion() {
+        return MAJOR + "." + MINOR + "." + BUILD;
+    }
+
+    public static String getName() {
+        return NAME;
+    }
+
+    public static int getBuild() {
+        return BUILD;
+    }
+
+    public static String getYear() {
+        return YEAR;
+    }
+}
